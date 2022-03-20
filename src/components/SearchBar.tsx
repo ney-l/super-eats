f9fd449eb3956ae6e9aa864f21444aacfcc22b7f
Ney Lanjewar <neylanjewar@gmail.com>
2022-03-20 21:59:57 +0530

    feat(Homescreen): adds Location Searchbar UI

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,69 @@
+import { View, StyleSheet, Text } from 'react-native';
+import React from 'react';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+export const SearchBar = () => {
+  return (
+    <View style={styles.container}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        query="test"
+        styles={searchBarStyles}
+        renderLeftButton={() => (
+          <View>
+            <Ionicons
+              name="location-sharp"
+              size={24}
+              style={styles.locationIcon}
+            />
+          </View>
+        )}
+        renderRightButton={() => (
+          <View style={styles.searchButton}>
+            <AntDesign name="clockcircle" size={11} style={styles.clockIcon} />
+            <Text>Search</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export const searchBarStyles = {
+  textInput: {
+    backgroundColor: '#eee',
+    borderRadius: 20,
+    fontWeight: '700',
+    marginTop: 7,
+  },
+  textInputContainer: {
+    backgroundColor: '#eee',
+    borderRadius: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15,
+    flexDirection: 'row',
+  },
+  locationIcon: {
+    marginLeft: 10,
+  },
+  searchButton: {
+    flexDirection: 'row',
+    marginRight: 8,
+    backgroundColor: 'white',
+    padding: 9,
+    borderRadius: 30,
+    alignItems: 'center',
+  },
+  clockIcon: {
+    marginRight: 6,
+  },
+});
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { HeaderTabs } from '../components/layout';
+import { SearchBar } from '../components/SearchBar';
 
 export function HomeScreen() {
   return (
     <View style={styles.container}>
       <View style={{ backgroundColor: 'white', padding: 15 }}>
         <HeaderTabs />
+        <SearchBar />
       </View>
     </View>
   );
