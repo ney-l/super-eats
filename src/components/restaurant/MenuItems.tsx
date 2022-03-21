c51a9771f4ce4f0f35fdb12c90c7ad17133bdd84
Ney Lanjewar <neylanjewar@gmail.com>
2022-03-22 02:07:03 +0530

    feat(Restaurant): adds menu items to restaurant details page

---
+++ src/components/restaurant/MenuItems.tsx
@@ -0,0 +1,102 @@
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import React from 'react';
+
+const foods = [
+  {
+    title: 'Lasagna',
+    description: 'With butter lettuce, tomato and sauce bechamel',
+    price: '$13.50',
+    image:
+      'https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg',
+  },
+  {
+    title: 'Tandoori Chicken',
+    description:
+      'Amazing Indian dish with tenderloin chicken off the sizzles 🔥',
+    price: '$19.20',
+    image: 'https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg',
+  },
+  {
+    title: 'Chilaquiles',
+    description:
+      'Chilaquiles with cheese and sauce. A delicious mexican dish 🇲🇽',
+    price: '$14.50',
+    image:
+      'https://i2.wp.com/chilipeppermadness.com/wp-content/uploads/2020/11/Chilaquales-Recipe-Chilaquiles-Rojos-1.jpg',
+  },
+  {
+    title: 'Chicken Caesar Salad',
+    description:
+      'One can never go wrong with a chicken caesar salad. Healthy option with greens and proteins!',
+    price: '$21.50',
+    image:
+      'https://images.themodernproper.com/billowy-turkey/production/posts/2019/Easy-italian-salad-recipe-10.jpg?w=1200&h=1200&q=82&fm=jpg&fit=crop&fp-x=0.5&fp-y=0.5&dm=1614096227&s=c0f63a30cef3334d97f9ecad14be51da',
+  },
+  {
+    title: 'Lasagna',
+    description: 'With butter lettuce, tomato and sauce bechamel',
+    price: '$13.50',
+    image:
+      'https://thestayathomechef.com/wp-content/uploads/2017/08/Most-Amazing-Lasagna-2-e1574792735811.jpg',
+  },
+];
+
+export const MenuItems = () => {
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      {foods.map((food, index) => (
+        <View key={`${food.title}-${index}`}>
+          <View style={styles.menuItems}>
+            <FoodInfo food={food} />
+            <FoodImage imageUrl={food.image} />
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+};
+
+const FoodInfo = ({
+  food,
+}: {
+  food: { title: string; description: string; price: string };
+}) => {
+  const { title, description, price } = food;
+  return (
+    <View style={styles.foodInfo}>
+      <Text style={styles.title}>{title}</Text>
+      <Text>{description}</Text>
+      <Text>{price}</Text>
+    </View>
+  );
+};
+
+const FoodImage = ({ imageUrl }: { imageUrl: string }) => (
+  <View>
+    <Image source={{ uri: imageUrl }} style={styles.foodImage} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  menuItems: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+    paddingVertical: 20,
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1,
+  },
+  foodInfo: {
+    width: 240,
+    justifyContent: 'space-evenly',
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: '600',
+  },
+  foodImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+});
--- src/screens/Restaurant.screen.tsx
+++ src/screens/Restaurant.screen.tsx
@@ -2,12 +2,14 @@ import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import { About } from '../components/restaurant/About';
 import { Divider } from 'react-native-elements';
+import { MenuItems } from '../components/restaurant/MenuItems';
 
 export const Restaurant = () => {
   return (
     <View>
       <About />
       <Divider width={1.8} style={styles.divider} />
+      <MenuItems />
     </View>
   );
 };
