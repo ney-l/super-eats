e9ef3beea15edf34aa569f2a50592a2ac934f819
Ney Lanjewar <neylanjewar@gmail.com>
2022-03-22 13:42:13 +0530

    refactor: removes unused import and organizes imports

--- src/screens/Restaurant.screen.tsx
+++ src/screens/Restaurant.screen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import React from 'react';
 import { About } from '../components/restaurant/About';
 import { Divider } from 'react-native-elements';
