44b2eccc228daca830dfcaea1d19ea7dc843d219
Ney Lanjewar <neylanjewar@gmail.com>
2022-03-23 23:52:46 +0530

    fix(OrderPreview): adds pollyfill for Andriod .toLocalString

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { RootNavigation } from './navigation';
 import { Provider as StoreProvider } from 'react-redux';
 import { store } from './store';
+import { Platform } from 'react-native';
+
+if (Platform.OS === 'android') {
+  require('intl');
+  require('intl/locale-data/jsonp/en-US');
+}
 
 export function App() {
   return (
